b80761158469d5e898e9a7cdb7cbd89fe33f69b9
puja12 <prabhupuja12@gmail.com>
2022-03-21 13:27:44 +0530

    images for service

--- src/app/service/serviceslist/serviceslist.component.ts
+++ src/app/service/serviceslist/serviceslist.component.ts
@@ -13,7 +13,6 @@ export class ServiceslistComponent implements OnInit {
     {id: '1',name:"Weight Loss", imgSrc: "assets/images/services/weight-loss.jpg"},
     {id: '2',name:"Weight Gain", imgSrc: "assets/images/services/weight-gain.jpg"},
     {id: '3',name:"Cholesterol Management", imgSrc:"assets/images/services/cholesterol_management.jpg"},
-    
     {id: '5',name:"Hypothyroid Management",imgSrc:"assets/images/services/hypothyroid_management.jpg"},
     {id: '6',name:"Personalized Diet",imgSrc:"assets/images/services/diet_plan.jpg"},
     {id: '7',name:"Sports Nutrition",imgSrc:"assets/images/services/sports-nutrition.jpg"},
