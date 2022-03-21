292d69c8f2f2af83459025ac808aa7c3f2276122
puja12 <prabhupuja12@gmail.com>
2022-03-21 13:03:25 +0530

    images for service

--- src/app/service/serviceslist/serviceslist.component.ts
+++ src/app/service/serviceslist/serviceslist.component.ts
@@ -10,16 +10,17 @@ export class ServiceslistComponent implements OnInit {
   constructor() { }
 
   service_list = [
-    {id: '1',name:"Weight Loss", imgSrc: "assets/images/services/what-we-offer-2.jpg"},
-    {id: '2',name:"Weight Gain", imgSrc: "assets/images/services/what-we-offer-2.jpg"},
-    {id: '3',name:"Cholesterol Management", imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '4',name:"Post Pregnancy Nutrition & Weight Loss",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '5',name:"Hypothyroid Management",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '6',name:"Personalized Diet",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '7',name:"Sports Nutrition",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '8',name:"Immunity Boosting",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
-    {id: '9',name:"Acidity Management",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
+    {id: '1',name:"Weight Loss", imgSrc: "assets/images/services/weight-loss.jpg"},
+    {id: '2',name:"Weight Gain", imgSrc: "assets/images/services/weight-gain.jpg"},
+    {id: '3',name:"Cholesterol Management", imgSrc:"assets/images/services/cholesterol_management.jpg"},
+    
+    {id: '5',name:"Hypothyroid Management",imgSrc:"assets/images/services/hypothyroid_management.jpg"},
+    {id: '6',name:"Personalized Diet",imgSrc:"assets/images/services/diet_plan.jpg"},
+    {id: '7',name:"Sports Nutrition",imgSrc:"assets/images/services/sports-nutrition.jpg"},
+    {id: '8',name:"Immunity Boosting",imgSrc:"assets/images/services/immunity-salad.jpg"},
+    {id: '9',name:"Acidity Management",imgSrc:"assets/images/services/acidity-management.jpg"},
     {id: '10',name:"LBS Management",imgSrc:"assets/images/services/what-we-offer-2.jpg"},
+    {id: '4',name:"Post Pregnancy Nutrition & Weight Loss",imgSrc:"assets/images/services/postpartum-diet-plan-for-weight-loss.jpg"},
   ];
 
   ngOnInit(): void {
