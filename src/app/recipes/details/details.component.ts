1fc7399b4234448cca816e003af768e90eac3f08
puja12 <prabhupuja12@gmail.com>
2022-03-24 16:41:26 +0530

    recipes

--- src/app/recipes/details/details.component.ts
+++ src/app/recipes/details/details.component.ts
@@ -9,6 +9,12 @@ export class DetailsComponent implements OnInit {
 
   constructor() { }
 
+  recipes=[
+    {rid:'1',imageSrc:'assets/images/meal_plan/early_morning_meethi_seeds.jpg',imgAlt:'',title:'Early Morning',total_time:'',serving:'',ingredients:'',steps:''},
+    {mid:'1',imageSrc:'assets/images/meal_plan/breakfast_protein-or-carbs.jpg',imgAlt:'',title:'Breakfast',total_time:'',serving:'',ingredients:'',steps:''},
+    {mid:'1',imageSrc:'assets/images/meal_plan/mid_morning.jpg',imgAlt:'',title:'Mid Morning',total_time:'',serving:'',ingredients:'',steps:''},
+  ];
+
   ngOnInit(): void {
   }
 
