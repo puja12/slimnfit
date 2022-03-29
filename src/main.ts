8704d3eab844668f2bc8e4f0fb7f29789256a267
puja12 <prabhupuja12@gmail.com>
2022-03-29 11:50:20 +0530

    ng bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {NgbdCollapseNavbarModule} from './app/collapse-navbar.module';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,4 +10,13 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+.then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
+
+  // Otherwise, log the boot error
+})
   .catch(err => console.error(err));
