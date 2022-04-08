882562582866adc8b7446608522b7d889ec53000
puja12 <prabhupuja12@gmail.com>
2022-04-09 00:14:01 +0530

    contact form successfull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
@@ -67,6 +69,8 @@ const appRoutes: Routes=[
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     CarouselModule,
     BrowserAnimationsModule
   ],
