b5e1acc4d5f6f09ee633dda27b818979641dfa10
puja12 <prabhupuja12@gmail.com>
2022-04-09 01:18:23 +0530

    contact form successfull

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -12,6 +12,7 @@ export class ContactusComponent implements OnInit {
   form: FormGroup;
   name: FormControl = new FormControl("", [Validators.required]);
   email: FormControl = new FormControl("", [Validators.required, Validators.email]);
+  phone_number: FormControl = new FormControl("", [Validators.required, Validators.pattern('^[0-9]*$'), Validators.maxLength(10)]);
   message: FormControl = new FormControl("", [Validators.required, Validators.maxLength(256)]);
   honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
   submitted: boolean = false; // show and hide the success message
@@ -22,9 +23,14 @@ export class ContactusComponent implements OnInit {
     this.form = this.formBuilder.group({
       name: this.name,
       email: this.email,
+      phone_number : this.phone_number,
       message: this.message,
       honeypot: this.honeypot
     });
+  }
+
+  get f(){
+    return this.form.controls;
   }
 
   ngOnInit(): void {
@@ -36,6 +42,7 @@ export class ContactusComponent implements OnInit {
       var formData: any = new FormData();
       formData.append("name", this.form.get("name")!.value);
       formData.append("email", this.form.get("email")!.value);
+      formData.append("phone_number", this.form.get("phone_number")!.value);
       formData.append("message", this.form.get("message")!.value);
       this.isLoading = true; // sending the post request async so it's in progress
       this.submitted = false; // hide the response message on multiple submits
@@ -43,7 +50,7 @@ export class ContactusComponent implements OnInit {
         (response) => {
           // choose the response message
           if (response['result'] == "success") {
-            this.responseMessage = "Thanks for the message! I'll get back to you soon!";
+            this.responseMessage = "Thanks for the message! We'll get back to you soon!";
           } else {
             this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
           }
