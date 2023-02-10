e632d45f77564c8deb40f0dedaed7f72bd867404
puja12 <prabhupuja12@gmail.com>
2023-02-10 18:49:37 +0530

    weight guru changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Slimnfit - Diet - Nutrition Health Center';
+  title = 'The Weight Guru - Diet & Nutrition Health Center';
 }
